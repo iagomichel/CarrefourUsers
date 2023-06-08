53ed098a314857baee61a32fba5a1f591e0e9aff
Iago M <iago.michelazevedo@gmail.com>
2023-06-08 20:11:23 -0300

    Adicionado UsersViewModel no modulo do Koin.

--- app/src/main/java/com/iagomichel/carrefourusers/di/CarrefourModules.kt
+++ app/src/main/java/com/iagomichel/carrefourusers/di/CarrefourModules.kt
@@ -1,7 +1,9 @@
 package com.iagomichel.carrefourusers.di
 
+import com.iagomichel.carrefourusers.viewmodel.UsersViewModel
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val carrefourModules = module {
-    //singleOf()
+    singleOf(::UsersViewModel)
 }
